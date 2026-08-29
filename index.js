7bdb83041e85f0ec3fac28998db331fed5d16ddf
MateMateus <mateusbragasan@gmail.com>
2026-08-29 09:51:23 -0300

    criado o primeiro servidor com o express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Olá, mundo!");
+import express from 'express'
+
+const app = express()
+// função callback
+app.get('/busca', (req, res) => {
+    console.log(req)
+  res.send('Olá mundo')
+})
+
+app.get('/busca/:id', (req, res) => {
+    const numero = req.params.id
+    console.log(numero)
+    const mensagem = `O número requisitado é: ${numero} `
+  res.send(mensagem)
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000')
+})
